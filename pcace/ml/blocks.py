0a792784f3ded2fdacb19cf0e4179b8719fc1ddf
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2026-04-17 11:16:12 -0400

    Added possibility for non-linear output layer

--- pcace/ml/blocks.py
+++ pcace/ml/blocks.py
@@ -1,8 +1,16 @@
+#****************************************************
+# Import Statements
+#****************************************************
+
 import torch
 from torch import nn
 from typing import Callable, Optional, Sequence
 
 __all__ = ["MLP"]
+
+#****************************************************
+# Multilayer Perceptron
+#****************************************************
 
 """
     Multilayer Perceptron
@@ -14,20 +22,23 @@ class MLP(nn.Module):
         n_in: int, n_out: int,
         n_hidden: Optional[Sequence[int]] = None,
         activation: Callable = nn.SiLU(),
+        linout: bool = True,
         bias: bool = True,
     ):
         super().__init__()
-        # ---- set the neural network architecture ----
+        # set the neural network architecture 
         if n_hidden is not None:
             n_neurons = [n_in] + n_hidden + [n_out]
         else: 
             n_neurons = [n_in] + [n_out]
-        # ---- create the layers ----
+        # create the layers 
         self.layers = nn.Sequential()
         for i in range(len(n_neurons)-2):
             self.layers.append(nn.Linear(n_neurons[i],n_neurons[i+1],bias))
             if(activation is not None): self.layers.append(activation)
+        # create the output layer
         self.layers.append(nn.Linear(n_neurons[-2],n_neurons[-1],bias))
+        if not linout: self.layers.append(activation)
         
     # ==== calculation ====
     def forward(self, input: torch.Tensor):
