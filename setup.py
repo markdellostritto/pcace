17cff80580bca480f1a8181bba00eaf60ca54ab8
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2026-07-24 12:08:36 -0400

    Relaxed external library version requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CMACE',
+    name='PCACE',
     version='0.1.0',
     author='Mark DelloStritto',
     author_email='mark.dellostritto@temple.edu',
     description='Cartesian Multilayer Atomic Cluster Expansion Machine Learning Potential',
     packages=find_packages(),
     install_requires=[
-        'numpy==1.26.4',
-        'ase==3.22.1',
-        'torch==2.6.0',
+        'numpy',
+        'ase',
+        'torch',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
