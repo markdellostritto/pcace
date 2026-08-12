f5966b867e23af58cee2e8d17817fcf6350f5161
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2026-08-12 10:36:41 -0400

    Added compute flags for edge forces
    Added compute flags to forward functions as well as flags for computing edge vectors for eventual integration into LAMMPS

--- pcace/mlp/nnp.py
+++ pcace/mlp/nnp.py
@@ -18,41 +18,46 @@ class NNP(torch.nn.Module):
         rep: torch.nn.Module = None, 
         # atomic neural network
         annl: torch.nn.ModuleList = None, 
-        # calc flags
-        calc_forces = True,
-        calc_virials = True,
-        calc_stress = True,
         # keys - energy/force
         key_energy = "energy_nnp",
         key_forces = "forces_nnp",
+        key_virials = "virials_nnp",
         key_stress = "stress_nnp",
+        key_forces_edge = "forces_edge_nnp",
     ):
         super().__init__()
         # set representation
         self.rep = rep
         # atomic neural network
         self.annl = annl
-        # set calc flags
-        self.calc_forces  = calc_forces
-        self.calc_virials = calc_virials
-        self.calc_stress  = calc_stress
-        for ann in self.annl:
-            ann.calc_force   = self.calc_forces
-            ann.calc_virials = self.calc_virials
-            ann.calc_stress  = self.calc_stress
         # set keys
-        self.key_energy=key_energy
-        self.key_forces=key_forces
-        self.key_stress=key_stress
+        self.key_energy = key_energy
+        self.key_forces = key_forces
+        self.key_virials = key_virials
+        self.key_stress = key_stress
+        self.key_forces_edge = key_forces_edge
 
     # ==== calculation ====
     def forward(
         self, 
         data: Dict[str, torch.Tensor],
         training = True,
+        compute_forces = True,
+        compute_stress = True,
+        compute_virials = True,
+        compute_forces_edge = False,
     ):
+        # == check compute flags ==
+        if not (compute_forces ^ compute_forces_edge):
+            raise ValueError("One must either compute forces based on positions OR vectors, not both.")
+        if compute_forces and "positions" not in data:
+            raise ValueError("Need positions to compute total atomic forces.")
+        if compute_forces_edge and "vectors" not in data:
+            raise ValueError("Need vectors to compute edge forces.")
+        
         # == set gradients required ==
-        data["positions"].requires_grad_(True)
+        if "positions" in data: data["positions"].requires_grad_(True)
+        if "vectors" in data: data["vectors"].requires_grad_(True)
 
         # == get the number of graphs ==
         try:
@@ -62,7 +67,7 @@ class NNP(torch.nn.Module):
         
         # == get symmetric displacement ==
         #print("get_symmetric_displacement")
-        if(self.calc_stress or self.calc_virials):
+        if(compute_stress or compute_virials):
             (
                 data["positions"],
                 data["shifts"],
@@ -88,14 +93,26 @@ class NNP(torch.nn.Module):
         energies = []
         forces = []
         stress = []
+        virials = []
+        forces_edge = []
         for ann in self.annl:
-            data = ann(data,training)
+            data = ann(data,
+                training,
+                compute_forces,
+                compute_stress,
+                compute_virials,
+                compute_forces_edge,
+            )
             energies.append(data[ann.key_energy])
-            forces.append(data[ann.key_forces])
-            stress.append(data[ann.key_stress])
+            if compute_forces: forces.append(data[ann.key_forces])
+            if compute_stress: stress.append(data[ann.key_stress])
+            if compute_virials: virials.append(data[ann.key_virials])
+            if compute_forces_edge: forces_edge.append(data[ann.key_forces_edge])
         data[self.key_energy]=torch.stack(energies).sum(0)
-        data[self.key_forces]=torch.stack(forces).sum(0)
-        data[self.key_stress]=torch.stack(stress).sum(0)
+        if compute_forces: data[self.key_forces]=torch.stack(forces).sum(0)
+        if compute_stress: data[self.key_stress]=torch.stack(stress).sum(0)
+        if compute_virials: data[self.key_virials]=torch.stack(virials).sum(0)
+        if compute_forces_edge: data[self.key_forces_edge]=torch.stack(forces_edge).sum(0)
 
         # == return the data ==
         return data
@@ -105,14 +122,12 @@ class NNP(torch.nn.Module):
         return (
             f"\n==============================================\n"
             f"{self.__class__.__name__}\n"
-            # calc flags
-            f"calc_forces = {self.calc_forces}\n"
-            f"calc_virials = {self.calc_virials}\n"
-            f"calc_stress = {self.calc_stress}\n"
             # keys - energy/force
             f"key_energy = {self.key_energy}\n"
             f"key_forces = {self.key_forces}\n"
             f"key_stress = {self.key_stress}\n"
+            f"key_virials = {self.key_virials}\n"
+            f"key_forces_edge = {self.key_forces_edge}\n"
             # representation
             f"rep = {self.rep}\n"
             # atomic neural network
