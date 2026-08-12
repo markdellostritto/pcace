d6cfd562616b172c87fc8fff44aa6b29f3ebb1e7
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2026-08-12 10:39:53 -0400

    Aligned with function names in code

--- pcace/data/__init__.py
+++ pcace/data/__init__.py
@@ -1,13 +1,13 @@
 from .molecule import Molecule, key_data_default, get_data_loader
 from .neighborhood import get_neighborhood
-from .load_data import random_train_valid_split, get_dataset_from_xyz, load_data_loader
+from .load_data import random_train_valid_split, read_dataset_xyz, load_data_loader
 
 __all__ = [
     "Molecule", 
     "key_data_default", 
     "get_neighborhood", 
     "get_data_loader",
-    "get_dataset_from_xyz",
+    "read_dataset_xyz",
     "load_data_loader",
     "random_train_valid_split",
 ]
