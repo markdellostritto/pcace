e9f896bf704c4e8a42b1839d5d34499511512948
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2026-09-01 11:40:43 -0400

    Moved all parameters to module
    Previously, there were variables which were not registered as parameters or buffers of the model.  All of those variables are now parameters or buffers, and redundant parameters have been removed.

--- pcace/mlp/cace.py
+++ pcace/mlp/cace.py
@@ -3,16 +3,12 @@
 #*************************************************************************************************
 
 import torch
-from torch import nn
 from typing import Sequence, List, Dict, Any
 
 from .type import (
     NodeEncoder, 
     NodeEmbedder, 
     EdgeEncoder,
-)
-from ..basis import (
-    AngularProduct
 )
 from ..tools import (
     scatter_sum
@@ -22,18 +18,17 @@ from ..tools import (
 # CACE Representation
 #*************************************************************************************************
 
-class CACE(nn.Module):
+class CACE(torch.nn.Module):
     # ==== initialization ====
     def __init__(
         self,
         # atomic numbers
         z_list: Sequence[int],
-        # body order
-        order: int,
         # basis
-        cutoff: nn.Module,
-        radial: nn.Module,
-        angular: nn.Module,
+        cutoff: torch.nn.Module,
+        radial: torch.nn.Module,
+        angular: torch.nn.Module,
+        angprod: torch.nn.Module,
         # node/edge encoding/embedding
         dim_node_embed: int,
         # radial embedding
@@ -44,25 +39,17 @@ class CACE(nn.Module):
         args_message_passing: Dict[str, Any] = {"M": {}, "Ar": {}, "Bchi": {}},
         avg_num_neighbors: float = 10.0,
         keep_node_features_A: bool = False,
-        # device
-        device: torch.device = torch.device("cpu"),
     ):
         # == init ==
         super().__init__()
 
-        # == device ==
-        self.device = device        
-            
         # == set constants and flags ==
-        self.mp_norm_factor = 1.0/(avg_num_neighbors)**0.5 # normalization factor for message passing
-        self.keep_node_features_A = keep_node_features_A
+        #self.mp_norm_factor = 1.0/(avg_num_neighbors)**0.5 # normalization factor for message passing
+        #self.keep_node_features_A = keep_node_features_A
         
         # == atomic numbers ==
         self.z_list = z_list # list of all possible elements
         self.nz = len(z_list) # number of possible elements
-        
-        # == body order ==
-        self.order = order
         
         # == node encoding ==
         self.node_encoder = NodeEncoder(self.z_list)
@@ -70,19 +57,22 @@ class CACE(nn.Module):
         # == node embedding ==
         self.dim_node_embed = dim_node_embed
         self.node_embedder_send = NodeEmbedder(
-            dim_node=self.nz, 
-            dim_embed=self.dim_node_embed, 
-            random_seed=42
+            dim_node    = self.nz, 
+            dim_embed   = self.dim_node_embed, 
+            random_seed = 42
         )
         self.node_embedder_recv = NodeEmbedder(
-            dim_node=self.nz, 
-            dim_embed=self.dim_node_embed, 
-            random_seed=42
+            dim_node    = self.nz, 
+            dim_embed   = self.dim_node_embed, 
+            random_seed = 42
         )
         
         # == edge encoding ==
         self.edge_encoder = EdgeEncoder(directed=True)
-        self.dim_edge_encode = self.dim_node_embed**2
+        #self.dim_edge_encode = self.dim_node_embed**2
+        self.register_buffer(
+            "dim_edge_encode", torch.tensor(self.dim_node_embed**2, dtype=torch.int)
+        )
 
         # == set cutoff ==
         self.cutoff = cutoff
@@ -90,27 +80,29 @@ class CACE(nn.Module):
         # == set radial basis ==
         self.radial = radial
         self.dim_radial = self.radial.nr
-        self.dim_radial_embed = dim_radial_embed
+        #self.dim_radial_embed = dim_radial_embed
+        self.register_buffer(
+            "dim_radial_embed", torch.tensor(dim_radial_embed, dtype=torch.int)
+        )
         
         # == set angular basis ==
         self.angular = angular
-        self.ang_lim = angular.vec_lim
         
         # == set radial transform ==
-        self.rt_weights=nn.ParameterList([
-            nn.Parameter(torch.rand([
+        self.rt_weights = torch.nn.ParameterList([
+            torch.nn.Parameter(torch.rand([
                 self.dim_radial, self.dim_radial_embed, self.dim_edge_encode
             ]),requires_grad=True) 
             for l in range(0,angular.l_max+1)
         ])
         
         # == set angular product ==
-        self.ang_prod=AngularProduct(order,angular.l_max)
+        self.angprod = angprod
 
         # == message passing ==
 
         # == set the input size ==
-        self.n_input = self.dim_radial_embed*self.ang_prod.size*self.dim_edge_encode
+        self.n_input = self.dim_radial_embed*self.angprod.size*self.dim_edge_encode
 
         
     # ==== calculation ====
@@ -118,28 +110,24 @@ class CACE(nn.Module):
         self, 
         data: Dict[str, torch.Tensor],
     ):
+        # == get the device ==
+        device = data["batch"].device
+        #print("device = ",device)
+
         # == get the network data ==
-        #print("get network data")
         n_nodes = data["atomic_numbers"].shape[0]
         #print("n_nodes = ",n_nodes)
-        if data["batch"] == None: batch_now = torch.zeros(n_nodes, dtype=torch.int64, device=self.device)
-        else: batch_now = data["batch"]
-        try:
-            data["num_graphs"]=data["ptr"].numel()-1
-        except:
-            data["num_graphs"]=1
-        #print("num_graphs = ",data["num_graphs"])
         
         # == node encoding ==
-        node_encoding = self.node_encoder(data["atomic_numbers"])
+        node_encoding = self.node_encoder(data["atomic_numbers"]) # [n_nodes, n_elements]
         #print("node_encoding = ",node_encoding)
-
+        
         # == node embedding ==
-        node_embedding_send = self.node_embedder_send(node_encoding)
-        node_embedding_recv = self.node_embedder_recv(node_encoding)
+        node_embedding_send = self.node_embedder_send(node_encoding) # [n_nodes, dim_node]
+        node_embedding_recv = self.node_embedder_recv(node_encoding) # [n_nodes, dim_node]
         #print("node_embedding_send = ",node_embedding_send)
         #print("node_embedding_recv = ",node_embedding_recv)
-
+        
         # == edge encoding == 
         # edge_encoding : [n_edges, dim_edge_encode]
         edge_encoding = self.edge_encoder(
@@ -148,7 +136,7 @@ class CACE(nn.Module):
             node_type_r=node_embedding_recv
         )
         #print("edge_encoding = ",edge_encoding)
-
+        
         # == compute edge lengths and vectors (normalized) ==
         # When running MD simulations (i.e. LAMMPS) the vectors already exist and 
         # should not be computed from the positions.  If the vectors are not already
@@ -158,7 +146,8 @@ class CACE(nn.Module):
                 - data["positions"][data["edge_index"][0]] \
                 + data["shifts"]  # [n_edges, 3]
         edge_lengths = torch.linalg.norm(data["vectors"], dim=-1, keepdim=True)  # [n_edges, 1]
-        edge_vectors = data["vectors"] / (edge_lengths + 1e-12)
+        edge_vectors = data["vectors"] / (edge_lengths + 1e-16) # [n_edges, 3]
+        #print("nedges = ",edge_lengths.size)
         
         # == compute angular and radial terms ==
         radial_component = self.radial(edge_lengths) # [n_edges, dim_radial]
@@ -182,7 +171,7 @@ class CACE(nn.Module):
             angular_component.unsqueeze(1).unsqueeze(3)*\
             edge_encoding.unsqueeze(1).unsqueeze(2)
         #print("edge_attri = ",edge_attri.size())
-
+        
         # == sum over edge features to each node ==
         # node_A : [n_nodes, dim_radial, dim_angular, dim_edge_encode]
         node_A = scatter_sum(
@@ -192,66 +181,69 @@ class CACE(nn.Module):
             dim_size=n_nodes
         )
         #print("node_A = ",node_A.size())
-
+        
         # == mix the different radial components ==
         # node_T : [n_nodes, dim_radial_embed, dim_angular, dim_edge_encode]
         node_T = torch.zeros((
-            n_nodes, 
-            self.dim_radial_embed, 
-            self.angular.size, 
+            n_nodes,
+            self.dim_radial_embed,
+            self.angular.size,
             self.dim_edge_encode),
-        device=self.device) 
+        device=device)
         for l, weight in enumerate(self.rt_weights):
             # set the beg and end to get the angular components with the same total angular momentum
-            lgroup = torch.arange(self.ang_lim[l][0], self.ang_lim[l][1])
+            lgroup = torch.arange(self.angular.lvec_lim[l][0], self.angular.lvec_lim[l][1],device=device)
             # Apply the transformation for all angular dims in the entire group at once
             node_T[:, :, lgroup, :] = torch.einsum('ijkh,jmh->imkh', node_A[:, :, lgroup, :], weight)
         #print("node_T = ",node_T.size())
             
         # == symmetrize the basis ==
         # node_S : [n_nodes, dim_radial_embed, dim_ang_prod, dim_edge_encode]
-        dim_ang_prod = self.ang_prod.size
+        dim_ang_prod = self.angprod.size
         node_S = torch.zeros((
             n_nodes, 
             self.dim_radial_embed, 
             dim_ang_prod, 
             self.dim_edge_encode),
-        device=self.device)
+        device=device)
 
-        # == symmetrize - full ==
-        if(self.order>=1):
-            #print(f"order >= 1 ({self.ang_prod.beg(1)},{self.ang_prod.end(1)})")
+        # == symmetrize ==
+        if(self.angprod.o_max>=1):
+            #print(f"o_max >= 1 ({self.angprod.beg(1)},{self.angprod.end(1)})")
             node_S[:, :, 0, :] = node_T[:, :, 0, :]
-        if(self.order>=2):
-            #print(f"order >= 2 ({self.ang_prod.beg(2)},{self.ang_prod.end(2)})")
-            for n in range(self.ang_prod.beg(2),self.ang_prod.end(2)):
-                lvec = self.ang_prod.lprod[n]
-                lim0 = torch.arange(self.ang_lim[lvec[0]][0],self.ang_lim[lvec[0]][1])
+        if(self.angprod.o_max>=2):
+            #print(f"o_max >= 2 ({self.angprod.beg(2)},{self.angprod.end(2)})")
+            for n in range(self.angprod.beg(2),self.angprod.end(2)):
+                lvec = self.angprod.lprod[n]
+                lvec_lim = self.angular.lvec_lim
+                lim0 = torch.arange(lvec_lim[lvec[0]][0],lvec_lim[lvec[0]][1],device=device)
                 node_S[:, :, n, :] = torch.einsum("abic,abic->abc",
                     node_T[:, :, lim0, :],
                     node_T[:, :, lim0, :]
                 )
-        if(self.order>=3):
-            #print(f"order >= 3 ({self.ang_prod.beg(3)},{self.ang_prod.end(3)})")
-            for n in range(self.ang_prod.beg(3),self.ang_prod.end(3)):
-                lvec = self.ang_prod.lprod[n]
-                lim0 = torch.arange(self.ang_lim[lvec[0]][0],self.ang_lim[lvec[0]][1])
-                lim1 = torch.arange(self.ang_lim[lvec[1]][0],self.ang_lim[lvec[1]][1])
-                lim0p1 = torch.arange(self.ang_lim[lvec[0]+lvec[1]][0],self.ang_lim[lvec[0]+lvec[1]][1])
+        if(self.angprod.o_max>=3):
+            #print(f"o_max >= 3 ({self.angprod.beg(3)},{self.angprod.end(3)})")
+            for n in range(self.angprod.beg(3),self.angprod.end(3)):
+                lvec = self.angprod.lprod[n]
+                lvec_lim = self.angular.lvec_lim
+                lim0 = torch.arange(lvec_lim[lvec[0]][0],lvec_lim[lvec[0]][1],device=device)
+                lim0p1 = torch.arange(lvec_lim[lvec[0]+lvec[1]][0],lvec_lim[lvec[0]+lvec[1]][1],device=device)
+                lim1 = torch.arange(lvec_lim[lvec[1]][0],lvec_lim[lvec[1]][1],device=device)
                 vshape0p1=node_T[:, :, lim0p1, :].shape
                 node_S[:, :, n, :] = torch.einsum("abic,abijc,abjc->abc",
                     node_T[:, :, lim0, :],
                     node_T[:, :, lim0p1, :].reshape(vshape0p1[0],vshape0p1[1],self.angular.l_size[lvec[0]],self.angular.l_size[lvec[1]],vshape0p1[3]),
                     node_T[:, :, lim1, :]
                 )
-        if(self.order>=4):
-            #print(f"order >= 4 ({self.ang_prod.beg(4)},{self.ang_prod.end(4)})")
-            for n in range(self.ang_prod.beg(4),self.ang_prod.end(4)):
-                lvec = self.ang_prod.lprod[n]
-                lim0 = torch.arange(self.ang_lim[lvec[0]][0],self.ang_lim[lvec[0]][1])
-                lim2 = torch.arange(self.ang_lim[lvec[2]][0],self.ang_lim[lvec[2]][1])
-                lim0p1 = torch.arange(self.ang_lim[lvec[0]+lvec[1]][0],self.ang_lim[lvec[0]+lvec[1]][1])
-                lim1p2 = torch.arange(self.ang_lim[lvec[1]+lvec[2]][0],self.ang_lim[lvec[1]+lvec[2]][1])
+        if(self.angprod.o_max>=4):
+            #print(f"o_max >= 4 ({self.angprod.beg(4)},{self.angprod.end(4)})")
+            for n in range(self.angprod.beg(4),self.angprod.end(4)):
+                lvec = self.angprod.lprod[n]
+                lvec_lim = self.angular.lvec_lim
+                lim0 = torch.arange(lvec_lim[lvec[0]][0],lvec_lim[lvec[0]][1],device=device)
+                lim0p1 = torch.arange(lvec_lim[lvec[0]+lvec[1]][0],lvec_lim[lvec[0]+lvec[1]][1],device=device)
+                lim1p2 = torch.arange(lvec_lim[lvec[1]+lvec[2]][0],lvec_lim[lvec[1]+lvec[2]][1],device=device)
+                lim2 = torch.arange(lvec_lim[lvec[2]][0],lvec_lim[lvec[2]][1],device=device)
                 vshape0p1=node_T[:, :, lim0p1, :].shape
                 vshape1p2=node_T[:, :, lim1p2, :].shape
                 node_S[:, :, n, :] = torch.einsum("abic,abijc,abjkc,abkc->abc",
@@ -268,22 +260,8 @@ class CACE(nn.Module):
         # == message passing ==
         # NOTE: TODO
         
-        # == build the output ==
-        try: displacement = data["displacement"]
-        except: displacement = None
-        output = {
-            "positions": data["positions"],
-            "vectors": data["vectors"],
-            "edge_index": data["edge_index"],
-            "atomic_numbers": data["atomic_numbers"],
-            "cell": data["cell"],
-            "displacement": displacement,
-            "batch": batch_now,
-            "node_feats": node_S,
-        }
-        
-        # == return the output ==
-        return output
+        # == return the features ==
+        return data
     
     # ==== output ====
     def __repr__(self):
@@ -291,11 +269,10 @@ class CACE(nn.Module):
             f"\n==============================================\n"
             f"{self.__class__.__name__}\n"
             f"z_list = {self.z_list}\n"
-            f"order  = {self.order}\n"
             f"cutoff = {self.cutoff}\n"
             f"radial  = {self.radial}\n"
             f"angular = {self.angular}\n"
-            f"product = {self.ang_prod}\n"
+            f"product = {self.angprod}\n"
             f"n_input = {self.n_input}\n"
             f"dim_node_embed   = {self.dim_node_embed}\n"
             f"dim_edge_encode  = {self.dim_edge_encode}\n"
@@ -303,7 +280,5 @@ class CACE(nn.Module):
             f"node_encoder = {self.node_encoder}\n"
             f"node_embedder_send = {self.node_embedder_send}\n"
             f"node_embedder_recv = {self.node_embedder_recv}\n"
-            f"device = {self.device}\n"
-            f"{self.rt_weights}\n"
             f"=============================================="
         )
